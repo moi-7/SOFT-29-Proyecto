a9309f04fb6679342c5188a891969b1724664d09
moi <187478441+moi-7@users.noreply.github.com>
2026-03-26 13:20:13 -0600

    Refactor: Migración de REACT SVG a HTML
    Se hace una migración del svg de react a vanilla html eliminando el uso de funciones y métodos como .map()

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const menuCheckbox = document.getElementById('menu_hamburguesa_interruptor');
-//   const navLinks = document.querySelectorAll('.nav-list-container a');
-//   const siteNav = document.querySelector('.site-nav-container');
+/*==========================================
+document.addEventListener('DOMContentLoaded', () => {
+  const menuCheckbox = document.getElementById('menu_hamburguesa_interruptor');
+  const navLinks = document.querySelectorAll('.nav-list-container a');
+  const siteNav = document.querySelector('.site-nav-container');
 
 
-//   navLinks.forEach(link => {
-//     link.addEventListener('click', () => {
-//       menuCheckbox.checked = false;
-//     });
-//   });
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      menuCheckbox.checked = false;
+    });
+  });
 
 
-//   document.addEventListener('click', (event) => {
-//     if (!menuCheckbox.checked) return;
-//     if (siteNav.contains(event.target)) return;
-    
-//     menuCheckbox.checked = false;
-//   });
-// });
+  document.addEventListener('click', (event) => {
+    if (!menuCheckbox.checked) return;
+    if (siteNav.contains(event.target)) return;
+
+    menuCheckbox.checked = false;
+  });
+});
+==========================================*/
